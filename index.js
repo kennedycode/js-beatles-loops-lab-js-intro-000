b5a5408bd1cc4a7cde5148682e836d00c09c9897
Andrew Kennedy <akennedycode@gmail.com>
2020-07-30 17:39:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var musiciansAndInstruments = []
+  for (let i = 0, l = musicians.length; i < l; i++) {
+    musiciansAndInstruments.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return musiciansAndInstruments
+}
+
+Function johnLennonFacts(facts)  {
+  const jLFacts = []
+  let i=0
+  while (i<facts.length) {
+    jLFacts.push('${facts[i]}!!!')
+    i++
+  }
+  return jLFacts
+}
